8db1d8fb3ad8e76cf7d1a89ace28bfe93914e757
Char <73309364+CharGoldenYT@users.noreply.github.com>
2025-03-20 16:25:50 -0400

    remove BGSpriteAlt

--- source/vschar/stages/MichealsForest.hx
+++ source/vschar/stages/MichealsForest.hx
@@ -1,14 +1,3 @@
 package vschar.stages;
 
-class MichealsForest extends BaseStage {
-    var trees:BGSpriteAlt;
-    
-    override function create() {
-        super.create();
-
-        if (subStage == '')
-        {
-            trees = newSprite(0, 0, 'tres'); // lmao.
-        }
-    }
-}
+class MichealsForest extends BaseStage {}
